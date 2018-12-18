9560a54bc385f29d7ef6c30fc742be1993a37763
jihoons8193 <jihoons8193@gmail.com>
2018-12-18 16:14:40 +0900

    no message

---
+++ Assets/BenchMark/GestureObjectLayout.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestureObjectLayout : MonoBehaviour
+{
+    public GameObject ObjTemplate;
+    private GameObject[] objArr;
+
+    private int screenWidth;
+    private int screenHeight;
+
+    private int objWidth;
+    private int objHeight;
+
+    // Use this for initialization
+    void Awake ()
+    {
+        screenWidth = 640;
+        screenHeight = 480;
+
+        objWidth = 100;
+        objHeight = 100;
+
+        var rowCount = screenWidth / objWidth;
+        var colCount = screenHeight / objHeight;
+
+        var rowCountHalf = rowCount / 2;
+        var colCountHalf = colCount / 2;
+
+        var objList = new List<GameObject>();
+
+        for (int i = -colCountHalf; i < colCountHalf; i++)
+        {
+            for (int j = -rowCountHalf; j < rowCountHalf; j++)
+            {
+                var obj = Instantiate(ObjTemplate, ObjTemplate.transform.parent);
+                obj.transform.localScale = new Vector3(objWidth, objHeight, 1);
+                obj.transform.localPosition = new Vector3(objWidth * j, objHeight * i, 0);
+
+                objList.Add(obj);
+            }
+        }
+
+        objArr = objList.ToArray();
+    }
+}
---
+++ Assets/BenchMark/PreviewCamCopy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreviewCamCopy : MonoBehaviour
+{
+    public Camera PreviewCam;
+    public RenderTexture Rt;
+
+	// Use this for initialization
+	IEnumerator Start ()
+    {
+        yield return new WaitForSeconds(3);
+        GetComponent<Camera>().CopyFrom(PreviewCam);
+        GetComponent<Camera>().targetTexture = Rt;
+
+    }
+}
